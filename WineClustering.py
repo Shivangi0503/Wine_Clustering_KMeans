833d6a56504845e59bb5126af2b15ffc11e081f2
Shivangi0503 <shivangisingh5march@gmail.com>
2021-01-27 00:33:09 +0100

    KMeans

---
+++ WineClustering.py
@@ -0,0 +1,138 @@
+import time
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn import metrics
+from sklearn.model_selection import ParameterGrid
+from sklearn.cluster import KMeans
+
+
+def load_embeddings():
+    """
+    Loading the wine dataset in pandas dataframe
+    :return: scaled data
+    """
+    # loading wine dataset
+    wine_raw = pd.read_csv("./data/wine-clustering.csv")
+
+    # checking data shape
+    row, col = wine_raw.shape
+    print(f'There are {row} rows and {col} columns') 
+    print(wine_raw.head(10))
+
+    # to work on copy of the data
+    wine_raw_scaled = wine_raw.copy()
+
+    # Scaling the data to keep the different attributes in same range.
+    wine_raw_scaled[wine_raw_scaled.columns] = StandardScaler().fit_transform(wine_raw_scaled)
+    print(wine_raw_scaled.describe())
+
+    return wine_raw_scaled
+
+
+def pca_embeddings(df_scaled):
+    """To reduce the dimensions of the wine dataset we use Principal Component Analysis (PCA).
+    Here we reduce it from 13 dimensions to 2.
+
+    :param df_scaled: scaled data
+    :return: pca result, pca for plotting graph
+    """
+
+    pca_2 = PCA(n_components=2)
+    pca_2_result = pca_2.fit_transform(df_scaled)
+    print('Explained variation per principal component: {}'.format(pca_2.explained_variance_ratio_))
+    print('Cumulative variance explained by 2 principal components: {:.2%}'.format(
+        np.sum(pca_2.explained_variance_ratio_)))
+    return pca_2_result, pca_2
+
+
+def kmean_hyper_param_tuning(data):
+    """
+    Hyper parameter tuning to select the best from all the parameters on the basis of silhouette_score.
+
+    :param data: dimensionality reduced data after applying PCA
+    :return: best number of clusters for the model (used for KMeans n_clusters)
+    """
+    # candidate values for our number of cluster
+    parameters = [2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40]
+
+    # instantiating ParameterGrid, pass number of clusters as input
+    parameter_grid = ParameterGrid({'n_clusters': parameters})
+
+    best_score = -1
+    kmeans_model = KMeans()     # instantiating KMeans model
+    silhouette_scores = []
+
+    # evaluation based on silhouette_score
+    for p in parameter_grid:
+        kmeans_model.set_params(**p)    # set current hyper parameter
+        kmeans_model.fit(data)          # fit model on wine dataset, this will find clusters based on parameter p
+
+        ss = metrics.silhouette_score(data, kmeans_model.labels_)   # calculate silhouette_score
+        silhouette_scores += [ss]       # store all the scores
+
+        print('Parameter:', p, 'Score', ss)
+
+        # check p which has the best score
+        if ss > best_score:
+            best_score = ss
+            best_grid = p
+
+    # plotting silhouette score
+    plt.bar(range(len(silhouette_scores)), list(silhouette_scores), align='center', color='#722f59', width=0.5)
+    plt.xticks(range(len(silhouette_scores)), list(parameters))
+    plt.title('Silhouette Score', fontweight='bold')
+    plt.xlabel('Number of Clusters')
+    plt.show()
+
+    return best_grid['n_clusters']
+
+
+def visualizing_results(pca_result, label, centroids_pca):
+    """ Visualizing the clusters
+
+    :param pca_result: PCA applied data
+    :param label: K Means labels
+    :param centroids_pca: PCA format K Means centroids
+    """
+    # ------------------ Using Matplotlib for plotting-----------------------
+    x = pca_result[:, 0]
+    y = pca_result[:, 1]
+
+    plt.scatter(x, y, c=label, alpha=0.5)  # plot different colors per cluster
+    plt.title('Wine clusters')
+    plt.xlabel('PCA 1')
+    plt.ylabel('PCA 2')
+
+    plt.scatter(centroids_pca[:, 0], centroids_pca[:, 1], marker='X', s=100, linewidths=1.5,
+                color='red', edgecolors="black", lw=1.5)
+
+    plt.show()
+
+
+def main():
+    print("1. Loading Wine dataset")
+    data_scaled = load_embeddings()
+
+    print("2. Reducing via PCA")
+    pca_result, pca_2 = pca_embeddings(data_scaled)
+
+    print("3. HyperTuning the Parameter for KMeans")
+    optimum_num_clusters = kmean_hyper_param_tuning(data_scaled)
+    print("optimum num of clusters =", optimum_num_clusters)
+
+    # fitting KMeans
+    kmeans = KMeans(n_clusters=optimum_num_clusters)
+    kmeans.fit(data_scaled)
+    centroids = kmeans.cluster_centers_
+    centroids_pca = pca_2.transform(centroids)
+
+    print("4. Visualizing the data")
+    visualizing_results(pca_result, kmeans.labels_, centroids_pca)
+
+
+if __name__ == "__main__":
+    main()
